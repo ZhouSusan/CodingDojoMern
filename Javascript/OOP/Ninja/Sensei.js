5eba075351ff3b8ea76117d80ea5ae027f00095d
Susan <szhou089@gmail.com>
2022-03-07 17:44:59 -0800

    "update"

--- Javascript/OOP/Ninja/Sensei.js
+++ Javascript/OOP/Ninja/Sensei.js
@@ -2,18 +2,14 @@ import {Ninja} from './Ninja.js';
 
 class Sensei extends Ninja {
     constructor(name) {
-        super(name);
-        this.strength = 10;
-        this.health = 100;
-        this.speed = 5;
+        super(name, 10, 100, 5);
         this.wisdom = 70;
     }
     speakWisdom() {
         console.log(`What one programmer can do in one month, two programmers can do in two months.`);
     }
     ChildFunction() {
-        const msg = super.showStats();
-        console.log(msg); 
+        super.showStats();
         console.log(`Wisdom: ${this.wisdom}`)
     }
 }
