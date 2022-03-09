47b12947dcc81bd2f45a93336d2db88875fba8ac
Susan <szhou089@gmail.com>
2022-03-08 18:14:20 -0800

    "update"

--- React/React/my-first-project/src/App.js
+++ React/React/my-first-project/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
       <ul>
         <li>Learn React</li>
         <li>Climb Mt. Everest</li>
